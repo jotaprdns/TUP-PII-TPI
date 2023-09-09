dbe3d43c7a6f463596ed94a01034d2649bee5a20
jotaprds <jordan.pradenas@gmail.com>
2023-09-08 22:47:32 -0300

    Se agrega funcion devolver libro

--- bibloteca.py
+++ bibloteca.py
@@ -26,6 +26,12 @@ def prestar_ejemplar_libro():
     buscar_codigo = input("Ingrese codigo del libro a prestar: ")
     for libro in libros:
         if buscar_codigo == libro["cod"]:
+            autor = libro["autor"]
+            titulo = libro["titulo"]
+            disponibles = libro["cant_ej_ad"]
+            print(f"Autor: {autor}")
+            print(f"Titulo: {titulo}")
+            print(f"Cantidad disponible: {disponibles}")
             if libro["cant_ej_ad"] == 0:
                 print("No hay ejemplares disponibles para prestamo")
             else:
@@ -41,5 +47,19 @@ def prestar_ejemplar_libro():
     return None
 
 def devolver_ejemplar_libro():
-    #completar
+    buscar_codigo = input("Ingrese codigo del libro a devolver: ")
+    for libro in libros:
+        if buscar_codigo == libro["cod"]:
+            if libro["cant_ej_pr"] == 0:
+                print("No hay registros de que se presto este libro, ingrese codigo nuevamente")
+            else:
+                libro["cant_ej_pr"] -= 1
+                print(f"Actualizado el stock de libros con codigo {buscar_codigo}, por lo que existen una stock libros prestados de: ")
+                print(libro["cant_ej_pr"])
+                libro["cant_ej_ad"] += 1
+                print(f"El total de libros disponibles para prestamo con el codigo {buscar_codigo} es de: ")
+                print(libro["cant_ej_ad"])
+            return
+    print(f"El codigo {buscar_codigo}, no se encuentra en la base de datos, verifique e intente nuevamente")
+    
     return None
